f717ffd00c7825e4e4cd10434f82de0657aa04ab
wegerle <github@wegerle.net>
2021-03-02 11:41:55 +0100

    fix copy paste issue else to elif

--- data/strongswan/5.9/alpine3.13/entrypoint.sh
+++ data/strongswan/5.9/alpine3.13/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ "$1" = 'run']; then
+if [ "$1" = 'run' ]; then
   /usr/sbin/ipsec start --nofork
     
-else [ "$1" = 'init']; then
+elif [ "$1" = 'init' ]; then
   /stronswan-init.sh
   
-else [ "$1" = 'lsusers']; then
+elif [ "$1" = 'lsusers' ]; then
   grep -v '^#' /etc/ipsec.secrets | cut -d '"' -f1 | cut -d '.' -f3
   
-else [ "$1" = 'adduser']; then
+elif [ "$1" = 'adduser' ]; then
   /strongswan-adduser.sh "$2" "$3" 
   /usr/sbin/ipsec reload
   
-#else [ "$1" = 'deluser']; then
+#elif [ "$1" = 'deluser' ]; then
 #  /strongswan-deluser.sh
 #  /usr/sbin/ipsec reload
   
