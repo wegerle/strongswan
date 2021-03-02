6942a4290444f5ff08a5355e685b918efac9813d
wegerle <github@wegerle.net>
2021-03-02 11:15:31 +0100

    Update Strongswan Version to 5.9.2 and adding some functions for the internal pki

---
+++ data/strongswan/5.9/alpine3.13/strongswan-adduser.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+CONFIG_DIR=$CONFIG_DIR
+
+if [ ! -n "${STRONGSWAN_CA_C}" ]; then
+  echo "The value for STRONGSWAN_CA_C is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_O}" ]; then
+  echo "The value for STRONGSWAN_CA_O is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_HOSTNAME}" ]; then
+  echo "The value for STRONGSWAN_HOSTNAME is missing."
+  exit 0
+fi
+
+#Cheick if Username is empty
+if [ -n "$2" ]; then
+
+  #Check if username exist
+  if [ grep -qw "$2" /etc/ipsec.secrets ]; then
+    #Check if password is empty
+    if [ -n "$3" ]; then
+      CLIENT_PASSWORD=$3
+    else
+      echo "No Password entered"
+      read -p "Should a password be generated? - (y/N)" generated
+      if [ "$generated" = 'y' ]; then
+        CLIENT_PASSWORD=$(pwgen -ysBv 15﻿)        
+        echo "########################################"
+        echo "#   The password for the ne user is:   #"
+        echo "#                                      #"
+        echo "#            $CLIENT_PASSWORD           #"
+        echo "#                                      #"
+        echo "########################################"
+      else
+        echo "Abort - No user added"
+        exit 0
+      fi
+    fi
+  else
+    echo "Abort - User exist"
+  fi
+  
+  if [ ! grep -qw "$2" /etc/ipsec.secrets  ] && [-n $CLIENT_PASSWORD]; then  
+    DOMAIN_NAME=$(echo ${STRONGSWAN_HOSTNAME} | cut -d "." -f2)
+    TLD=$(echo ${STRONGSWAN_HOSTNAME} | cut -d "." -f3)
+    CLIENT_CN="$2"@"$DOMAIN_NAME"."$TLD"
+    
+    if [ "${STRONGSWAN_CLIENT_KEY_TYPE}" = 'RSA2048' ]; then
+      pki --gen --type rsa --size 2048 --outform pem > $CONFIG_DIR/private/"$2"_Key.pem
+      
+    elif [ "${STRONGSWAN_CLIENT_KEY_TYPE}" = 'RSA3072' ]; then
+      pki --gen --type rsa --size 3072 --outform pem > $CONFIG_DIR/private/"$2"_Key.pem
+      
+    elif [ "${STRONGSWAN_CLIENT_KEY_TYPE}" = 'RSA4096' ]; then
+      pki --gen --type rsa --size 4096 --outform pem > $CONFIG_DIR/private/"$2"_Key.pem
+      
+    elif [ "${STRONGSWAN_CLIENT_KEY_TYPE}" = 'ECDSA256' ]; then
+      pki --gen --type ecdsa --size 256 --outform pem > $CONFIG_DIR/private/"$2"_Key.pem
+      
+    elif [ "${STRONGSWAN_CLIENT_KEY_TYPE}" = 'ECDSA384' ]; then
+      pki --gen --type ecdsa --size 384 --outform pem > $CONFIG_DIR/private/"$2"_Key.pem
+      
+    elif [ "${STRONGSWAN_CLIENT_KEY_TYPE}" = 'ECDSA521' ]; then
+      pki --gen --type ecdsa --size 521 --outform pem > $CONFIG_DIR/private/"$2"_Key.pem
+    fi
+    
+    echo \"$CLIENT_CN\"" : EAP \""\"$CLIENT_PASSWORD\""\"" >> /etc/ipsec.secrets
+    
+    pki --issue --in $CONFIG_DIR/private/"$2"_Key.pem --type priv --cacert $CONFIG_DIR/cacerts/caCert.pem --cakey $CONFIG_DIR/private/caKey.pem \
+          --dn "C=${STRONGSWAN_CA_C}, O=${STRONGSWAN_CA_O}, CN=$CLIENT_CN" --san=\"$CLIENT_CN\" --outform pem > $CONFIG_DIR/certs/"$2"_Cert.pem
+          
+    openssl pkcs12 -export -inkey $CONFIG_DIR/private/"$2"_Key.pem -in $CONFIG_DIR/certs/"$2"_Cert.pem -name \"$CLIENT_CN\" -certfile $CONFIG_DIR/cacerts/caCert.pem \
+                   -caname \"$CA_CN\" -out $CONFIG_DIR/"$2"_Cert.p12 -p \"$CLIENT_PASSWORD\"
+  fi
+else
+  echo "The value for client username is missing."
+  echo "Abort - No user added"
+fi
+  
---
+++ data/strongswan/5.9/alpine3.13/strongswan-init.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+CONFIG_DIR=/etc/ipsec.d
+
+if [ ! -n "${STRONGSWAN_CA_CN}" ]; then
+  echo "The value for STRONGSWAN_CA_CN is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_C}" ]; then
+  echo "The value for STRONGSWAN_CA_C is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_O}" ]; then
+  echo "The value for STRONGSWAN_CA_O is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_OU}" ]; then
+  echo "The value for STRONGSWAN_CA_OU is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_L}" ]; then
+  echo "The value for STRONGSWAN_CA_L is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_ST}" ]; then
+  echo "The value for STRONGSWAN_CA_ST is missing."
+  exit 0
+fi
+if [ ! -n "${STRONGSWAN_CA_SAN}" ]; then
+  echo "The value for STRONGSWAN_CA_SAN is missing."
+  exit 0
+fi
+
+if [ -n "${STRONGSWAN_CA_KEY_TYPE}" ]; then
+  for dir in \
+          "$CONFIG_DIR/aacerts" \
+          "$CONFIG_DIR/acerts" \
+          "$CONFIG_DIR/cacerts" \
+          "$CONFIG_DIR/certs" \
+          "$CONFIG_DIR/crls" \
+          "$CONFIG_DIR/ocspcerts" \
+          "$CONFIG_DIR/private"
+  do
+    if [ ! -d "$dir" ]; then
+        mkdir -p "$dir"
+    fi
+  done
+  
+  if [ ! -f "/etc/ipsec.secrets" ]; then
+    touch /etc/ipsec.secrets
+  fi
+           
+  if [ "${STRONGSWAN_CA_KEY_TYPE}" = 'RSA2048' ]; then
+    pki --gen --type rsa --size 2048 --outform pem > $CONFIG_DIR/private/caKey.pem
+    pki --gen --type rsa --size 2048 --outform pem > $CONFIG_DIR/private/serverKey.pem
+    echo ": RSA serverKey.pem" >> /etc/ipsec.secrets
+    
+  elif [ "${STRONGSWAN_CA_KEY_TYPE}" = 'RSA3072' ]; then
+    pki --gen --type rsa --size 3072 --outform pem > $CONFIG_DIR/private/caKey.pem
+    pki --gen --type rsa --size 3072 --outform pem > $CONFIG_DIR/private/serverKey.pem
+    echo ": RSA serverKey.pem" >> /etc/ipsec.secrets
+    
+  elif [ "${STRONGSWAN_CA_KEY_TYPE}" = 'RSA4096' ]; then
+    pki --gen --type rsa --size 4096 --outform pem > $CONFIG_DIR/private/caKey.pem
+    pki --gen --type rsa --size 4096 --outform pem > $CONFIG_DIR/private/serverKey.pem
+    echo ": RSA serverKey.pem" >> /etc/ipsec.secrets
+    
+  elif [ "${STRONGSWAN_CA_KEY_TYPE}" = 'ECDSA256' ]; then
+    pki --gen --type ecdsa --size 256 --outform pem > $CONFIG_DIR/private/caKey.pem
+    pki --gen --type ecdsa --size 256 --outform pem > $CONFIG_DIR/private/serverKey.pem
+    echo ": ECDSA  serverKey.pem" >> /etc/ipsec.secrets
+    
+  elif [ "${STRONGSWAN_CA_KEY_TYPE}" = 'ECDSA384' ]; then
+    pki --gen --type ecdsa --size 384 --outform pem > $CONFIG_DIR/private/caKey.pem
+    pki --gen --type ecdsa --size 384 --outform pem > $CONFIG_DIR/private/serverKey.pem
+    echo ": ECDSA  serverKey.pem" >> /etc/ipsec.secrets
+    
+  elif [ "${STRONGSWAN_CA_KEY_TYPE}" = 'ECDSA521' ]; then
+    pki --gen --type ecdsa --size 521 --outform pem > $CONFIG_DIR/private/caKey.pem
+    pki --gen --type ecdsa --size 521 --outform pem > $CONFIG_DIR/private/serverKey.pem
+    echo ": ECDSA  serverKey.pem" >> /etc/ipsec.secrets
+  fi
+  
+  pki --self --in $CONFIG_DIR/private/caKey.pem \
+      --dn "CN=${STRONGSWAN_CA_CN}, C=${STRONGSWAN_CA_C}, O=${STRONGSWAN_CA_O}, OU=${STRONGSWAN_CA_OU}, L=${STRONGSWAN_CA_L}, ST=${STRONGSWAN_CA_ST}" \
+      --ca --outform pem > $CONFIG_DIR/cacerts/caCert.pem
+  
+  pki --issue --in $CONFIG_DIR/private/serverKey.pem --type priv --cacert $CONFIG_DIR/cacerts/caCert.pem --cakey $CONFIG_DIR/private/caKey.pem \
+      --dn "CN=${STRONGSWAN_CA_CN}, C=${STRONGSWAN_CA_C}, O=${STRONGSWAN_CA_O}, OU=${STRONGSWAN_CA_OU}, L=${STRONGSWAN_CA_L}, ST=${STRONGSWAN_CA_ST}" \
+      --san="${STRONGSWAN_CA_SAN}" --flag serverAuth --flag ikeIntermediate --outform pem > $CONFIG_DIR/certs/serverCert.pem
+      
+  echo "CA-Key/Cert and Server-Key/Cert are successfully created."
+
+else
+  echo "The value for STRONGSWAN_CA_KEY_TYPE is missing."
+  exit 0
+fi
