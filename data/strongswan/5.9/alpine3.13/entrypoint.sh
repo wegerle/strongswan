e9e7454fa3def2f3d1a9b8f48de519795709162d
wegerle <github@wegerle.net>
2021-03-04 08:28:51 +0100

    Renaming files

--- data/strongswan/5.9/alpine3.13/entrypoint.sh
+++ data/strongswan/5.9/alpine3.13/entrypoint.sh
@@ -4,17 +4,17 @@ if [ "$1" = 'run' ]; then
   /usr/sbin/ipsec start --nofork
     
 elif [ "$1" = 'strongswan-init' ]; then
-  /stronswan-init.sh
+  /strongswan-init
   
 elif [ "$1" = 'strongswan-lsusers' ]; then
   grep -v '^#' /etc/ipsec.secrets | cut -d '"' -f1 | cut -d '.' -f3
   
 elif [ "$1" = 'strongswan-useradd' ]; then
-  /strongswan-useradd.sh "$2" "$3" 
+  /strongswan-useradd "$2" "$3" 
   /usr/sbin/ipsec reload
   
 #elif [ "$1" = 'strongswan-userdel' ]; then
-#  /strongswan-userdel.sh
+#  /strongswan-userdel
 #  /usr/sbin/ipsec reload
   
 fi
